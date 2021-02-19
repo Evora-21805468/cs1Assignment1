23e265b6038ba3e65bb406c352dae068f4075ff3
Evora-21805468 <56166747+evora-21805468@users.noreply.github.com>
2021-02-19 19:08:05 +0000

    data maybe working?....

--- src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
+++ src/pt/ulusofona/cm/kotlin/challenge/models/Pessoa.kt
@@ -11,7 +11,6 @@ import java.time.LocalDate
 import java.time.Period
 import java.util.Arrays
 import java.util.Date
-import kotlin.time.days
 
 data class Pessoa(var nome: String, var dataDeNascimento: Date) : Movimentavel {
 
